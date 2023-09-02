8fe204247aaf7e0b749623c0b2fdb35f9f8de865
odcenteno <pixie_cemodan@hotmail.com>
2023-09-02 12:56:55 -0700

    login journey tested with POM

--- cypress.config.js
+++ cypress.config.js
@@ -2,12 +2,12 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    loginURL: 'https://naveenautomationlabs.com/opencart/index.php?route=account/login',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
   env: {
+    loginURL: 'https://naveenautomationlabs.com/opencart/index.php?route=account/login',
     CV_URL: 'https://www.quickcurriculum.com/',
     URL: 'https://naveenautomationlabs.com/opencart/'
   }
