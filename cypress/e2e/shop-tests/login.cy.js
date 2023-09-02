83ae7b7bdbe7d8270bf423c913842b61d4f8ab14
odcenteno <pixie_cemodan@hotmail.com>
2023-09-02 12:57:20 -0700

    login journey set with POM

---
+++ cypress/e2e/shop-tests/login.cy.js
@@ -0,0 +1,24 @@
+import { NewHomePage } from "../../pages/home.page";
+import userData from '../../fixtures/userData.json'
+
+describe('Home Page testing suite', () => { 
+  beforeEach(() => {
+    NewHomePage.openURL();
+  }) 
+  context('Login process with POM', () => {
+    it('should login', () => {
+      // NewHomePage.openURL();
+      NewHomePage.typeEmail(userData.email);
+      NewHomePage.typePassword(userData.password);
+      NewHomePage.clicLogin();
+      NewHomePage.confirmAccess()
+    })
+  })
+
+  context('New user button', () => {
+    it('should take the user to the registration page', () => {
+      //NewHomePage.openURL();
+      NewHomePage.isNewCustomer();
+    })
+  }) 
+})
--- cypress/pages/home.page.js
+++ cypress/pages/home.page.js
@@ -1,18 +1,38 @@
 export class HomePage {
   webLocators = {
     searchInput: 'search',
-    password: '#input-password'
+    email: '#input-email',
+    password: '#input-password',
+    loginButton: '[value="Login"]',
+    registrationBtn: '.well > a'
   }
 
-  typeEmail() {
-    //
+  openURL() {
+    cy.visit(Cypress.env('loginURL'));
+    cy.location('search').should('eql', '?route=account/login')
   }
 
-  typePassword() {
+  typeEmail(email) {
+    cy.get(this.webLocators.email).type(email);
+  }
 
+  typePassword(password) {
+    cy.get(this.webLocators.password).type(password);
   }
 
   clicLogin() {
+    cy.get(this.webLocators.loginButton).click();
+  }
 
+  confirmAccess() {
+    cy.location('search').should('eql', '?route=account/account')
+  }
+
+  isNewCustomer() {
+    cy.get(this.webLocators.registrationBtn).click();
+    cy.location('search').should('eql', '?route=account/register');
+    cy.contains('Register Account');
   }
 }
+
+export const NewHomePage = new HomePage();
